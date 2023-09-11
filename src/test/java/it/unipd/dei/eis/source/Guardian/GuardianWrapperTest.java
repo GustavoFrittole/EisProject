5e4831a5d42742f00da3c6c9eb5f9c4225980915
SadanoRapa <gabriele.callegaro@gmail.com>
2023-09-11 21:06:24 +0200

    ultimi ritocchi

--- src/test/java/it/unipd/dei/eis/source/Guardian/GuardianWrapperTest.java
+++ src/test/java/it/unipd/dei/eis/source/Guardian/GuardianWrapperTest.java
@@ -7,12 +7,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class GuardianWrapperTest {
     //Difficile da rendere autonoma dall' API client senza modifiche
     //  strutturali non desiderabili
-    private static String yorApiKey = "";
+    private static String yourApiKey = "";
 
     @Test
     void retriveArticlesExampleTest() {
         int artPerPage = 3, totPages = 3;
-        GuardianWrapper guardianWrapper = new GuardianWrapper(yorApiKey, "random query", artPerPage, totPages);
+        GuardianWrapper guardianWrapper = new GuardianWrapper(yourApiKey, "random query", artPerPage, totPages);
         guardianWrapper.retriveArticles();
         for (int i = 0; i < artPerPage * totPages; i++)
             assertNotNull(guardianWrapper.getArticle(0));
@@ -23,7 +23,7 @@ class GuardianWrapperTest {
 
     void retriveArticlesTooManyPagesTest() {
         int bigNumber = 10000;
-        GuardianWrapper guardianWrapper = new GuardianWrapper(yorApiKey, "random query", 200, 10000);
+        GuardianWrapper guardianWrapper = new GuardianWrapper(yourApiKey, "random query", 200, 10000);
         guardianWrapper.retriveArticles();
         assertTrue(guardianWrapper.getTotPages() < bigNumber);
     }
